41dca60bcef4b2f9b210307df5b6da2f218ecdcb
kat055 <gacusankatherine0514@gmail.com>
2022-03-23 11:01:03 -0700

    navbar

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,10 +3,27 @@
 </script>
 
 <template>
-<div>
+<div class="bg-pink-100 h-screen w-screen py-10">
 
-<div class="text-center">
-  Medora
+<div class="flex flex-row">
+    <div class="px-10
+                text-lg font-bold 
+    ">
+        Furniture
+    </div>
+
+    <form action="search">
+        <input type="text" placeholder="SEARCH"
+        class="rounded px-4 ">
+    </form>
+    
+    <div class="flex flex-row gap-16 pl-16 py-2
+                font-bold text-sm hover:underline
+    ">
+        <div><a href="">Home</a></div>
+        <div><a href="">Stores</a></div>
+        <div><a href="">Contact</a></div>
+    </div>
 </div>
 
 </div>
